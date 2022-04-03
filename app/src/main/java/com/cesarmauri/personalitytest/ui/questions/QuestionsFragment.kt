9533292d04079e9f58e8e9168e973ce2fdf692c6
Cesar Mauri <cesar@crea-si.com>
2022-04-03 20:17:54 +0200

    Create questions screen layout and data binding

--- app/src/main/java/com/cesarmauri/personalitytest/ui/questions/QuestionsFragment.kt
+++ app/src/main/java/com/cesarmauri/personalitytest/ui/questions/QuestionsFragment.kt
@@ -5,9 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.cesarmauri.personalitytest.R
 import com.cesarmauri.personalitytest.databinding.FragmentQuestionsBinding
 
 class QuestionsFragment : Fragment() {
@@ -27,12 +28,74 @@ class QuestionsFragment : Fragment() {
             _binding = binding
             val root: View = binding.root
 
-            val textView: TextView = binding.textQuestions
-            questionsViewModel.text.observe(viewLifecycleOwner, Observer {
-                textView.text = it
-            })
+            setObservers(binding)
+            setListener(binding)
+
             return root
         }
+    }
+
+    private fun setObservers(binding: FragmentQuestionsBinding) {
+        questionsViewModel.questionNumber.observe(viewLifecycleOwner, {
+            binding.questionNumber.text = it
+        })
+
+        questionsViewModel.questionStatement.observe(viewLifecycleOwner, {
+            binding.questionStatement.text = it
+        })
+
+        questionsViewModel.answer1.observe(viewLifecycleOwner, {
+            binding.answer1.text = "A: $it"
+        })
+
+        questionsViewModel.answer2.observe(viewLifecycleOwner, {
+            binding.answer2.text = "B: $it"
+        })
+
+        questionsViewModel.answer3.observe(viewLifecycleOwner, {
+            binding.answer3.text = "C: $it"
+        })
+
+        questionsViewModel.answer4.observe(viewLifecycleOwner, {
+            binding.answer4.text = "D: $it"
+        })
+
+        questionsViewModel.hasPreviousQuestion.observe(viewLifecycleOwner, {
+            binding.buttonPrevious.visibility = if (it) View.VISIBLE else View.GONE
+        })
+
+        questionsViewModel.hasNextQuestion.observe(viewLifecycleOwner, {
+            binding.buttonNext.visibility = if (it) View.VISIBLE else View.GONE
+        })
+
+        questionsViewModel.canGoNext.observe(viewLifecycleOwner, {
+            binding.buttonNext.isEnabled = it
+        })
+
+        questionsViewModel.selectedAnswer.observe(viewLifecycleOwner, {
+            for ((i, answer) in answersAsArray(binding).withIndex()) {
+                if (i == it - 1) selectAnswer(answer)
+                else unSelectAnswer(answer)
+            }
+        })
+    }
+
+    private fun setListener(binding: FragmentQuestionsBinding) {
+        for ((i, answer) in answersAsArray(binding).withIndex()) {
+            answer.setOnClickListener { questionsViewModel.updateSelectedAnswer(i+1) }
+        }
+    }
+
+    private fun selectAnswer(answer: TextView) {
+        answer.background = ContextCompat.getDrawable(requireContext(), R.drawable.back_selected)
+    }
+
+    private fun unSelectAnswer(answer: TextView) {
+        answer.background = ContextCompat.getDrawable(requireContext(), R.drawable.back_not_selected)
+    }
+
+    private fun answersAsArray(binding: FragmentQuestionsBinding): Array<TextView> {
+        return arrayOf(binding.answer1, binding.answer2, binding.answer3, binding.answer4)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/cesarmauri/personalitytest/ui/questions/QuestionsViewModel.kt
+++ app/src/main/java/com/cesarmauri/personalitytest/ui/questions/QuestionsViewModel.kt
@@ -6,8 +6,59 @@ import androidx.lifecycle.ViewModel
 
 class QuestionsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is questions Fragment"
+    private val _questionNumber = MutableLiveData<String>().apply {
+        value = "Question 1 of 10"
     }
-    val text: LiveData<String> = _text
+
+    private val _questionStatement = MutableLiveData<String>().apply {
+        value = "This is the question statament. Lorem fistrum hasta luego Lucas papaar papaar " +
+                "hasta luego Lucas apetecan benemeritaar quietooor."
+    }
+
+    private val _answer1 = MutableLiveData<String>().apply {
+        value = "This is the first answer"
+    }
+
+    private val _answer2 = MutableLiveData<String>().apply {
+        value = "This is the second answer"
+    }
+
+    private val _answer3 = MutableLiveData<String>().apply {
+        value = "This is the third answer"
+    }
+
+    private val _answer4 = MutableLiveData<String>().apply {
+        value = "This is the fourth answer"
+    }
+
+    private val _hasPreviousQuestion = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+
+    private val _hasNextQuestion = MutableLiveData<Boolean>().apply {
+        value = true
+    }
+
+    private val _canGoNext = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+
+    private val _selectedAnswer = MutableLiveData<Int>().apply {
+        value = 0
+    }
+
+    val questionNumber: LiveData<String> = _questionNumber
+    val questionStatement: LiveData<String> = _questionStatement
+    val answer1: LiveData<String> = _answer1
+    val answer2: LiveData<String> = _answer2
+    val answer3: LiveData<String> = _answer3
+    val answer4: LiveData<String> = _answer4
+    val hasPreviousQuestion: LiveData<Boolean> = _hasPreviousQuestion
+    val hasNextQuestion: LiveData<Boolean> = _hasNextQuestion
+    val canGoNext: LiveData<Boolean> = _canGoNext
+    val selectedAnswer: LiveData<Int> = _selectedAnswer
+    fun updateSelectedAnswer(i: Int) {
+        _selectedAnswer.value = i
+        _canGoNext.value = true
+    }
 }
