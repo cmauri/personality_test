b4a5fe5cdaa5dde77720372ddfadf1b64272ee83
Cesar Mauri <cesar@crea-si.com>
2022-04-04 20:01:42 +0200

    Add results fragment

---
+++ app/src/main/java/com/cesarmauri/personalitytest/domain/commands/NavigationCommand.kt
@@ -0,0 +1,7 @@
+package com.cesarmauri.personalitytest.domain.commands
+
+import com.cesarmauri.personalitytest.R
+
+sealed class NavigationCommand(val id: Int) {
+    object Questions: NavigationCommand(R.id.navigation_questions)
+}
---
+++ app/src/main/java/com/cesarmauri/personalitytest/ui/fragments/ResultFragment.kt
@@ -0,0 +1,53 @@
+package com.cesarmauri.personalitytest.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.cesarmauri.personalitytest.R
+import com.cesarmauri.personalitytest.databinding.FragmentResultBinding
+import com.cesarmauri.personalitytest.ui.questions.QuestionsViewModel
+
+class ResultFragment: Fragment() {
+    private val questionsViewModel: QuestionsViewModel by activityViewModels()
+    private var _binding: FragmentResultBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        FragmentResultBinding.inflate(inflater, container, false).let { binding ->
+            _binding = binding
+            val root: View = binding.root
+
+            questionsViewModel.isExtrovert.observe(viewLifecycleOwner) { isExtrovert ->
+                if (isExtrovert) {
+                    binding.resultTitle.setText(R.string.title_you_are_extrovert)
+                    binding.imageExtrovert.visibility = View.VISIBLE
+                    binding.imageIntrovert.visibility = View.GONE
+                }
+                else {
+                    binding.resultTitle.setText(R.string.title_you_are_introvert)
+                    binding.imageExtrovert.visibility = View.GONE
+                    binding.imageIntrovert.visibility = View.VISIBLE
+                }
+            }
+
+            questionsViewModel.score.observe(viewLifecycleOwner) { score ->
+                binding.resultScore.text = getString(R.string.result_your_score, score)
+            }
+
+            return root
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
